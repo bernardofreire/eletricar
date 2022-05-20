92266eca6e147a3a79055f0517195528513612c1
bernardofreire <b.bernardo9815@gmail.com>
2022-05-20 01:24:33 -0300

    Correção de bug no scroll

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,34 @@
-const openMenu = () =>{
-    document.querySelector('.backdrop').className = 'backdrop active'
-   document.querySelector('aside').className = 'active'
+function disableScroll(){
+    document.body.style.overflow = 'hidden'
+    document.getElementById('sidebar').style.overflow = 'auto'  
 }
-const closeMenu = () =>{
+
+function enableScroll(){
+    document.body.style.overflow = 'overlay'
+    
+}
+
+const openMenu = () => {
+    document.querySelector('.backdrop').className = 'backdrop active'
+    document.querySelector('aside').className = 'active'
+    disableScroll()
+}
+const closeMenu = () => {
     document.querySelector('.backdrop').className = 'backdrop'
     document.querySelector('aside').className = ''
+    enableScroll()
+}
 
-}   
-
-document.getElementById('menuBtn').onclick = e =>{
+document.getElementById('menuBtn').onclick = e => {
     e.preventDefault()
     openMenu()
 }
 
 
-document.querySelector('aside button.close').onclick = e =>{
+document.querySelector('aside button.close').onclick = e => {
     closeMenu()
 }
 
-document.querySelector('.backdrop').onclick = e =>{
+document.querySelector('.backdrop').onclick = e => {
     closeMenu()
 }
